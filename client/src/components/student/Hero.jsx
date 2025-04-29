c758635329e14e5469a06e98eeb06e977b3b466e
ImHamza22 <ImHamza63@gmail.com>
2025-04-30 04:30:53 +0500

    Updated

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -12,9 +12,10 @@ const Hero = () => {
 
         <img src={assets.sketch} alt="sketch" className='md:block hidden absolute -bottom-7 right-0' /></h1>
 
-        <p className='md:block hidden text-gray-500 max-w-2x1 mx-auto'> Unlock Your Potential with Expert-Led Courses. Welcome to <b>SkillSync,</b> your all-in-one learning platform where knowledge meets opportunity. Whether you're looking to boost your career, learn a new skill, or explore your passion, we offer a wide range of expertly crafted courses to help you succeed—anytime, anywhere. Start learning today and take control of your future.</p>
+        <p className='md:block hidden text-gray-500 max-w-2x1 mx-auto'> Unlock Your Potential with Expert-Led Courses. Welcome to <b>SkillSync,</b> your all-in-one learning platform where knowledge meets opportunity. Whether
+        you're looking to boost your career or learn a new skill, <br/>we offer a wide range of expertly crafted courses to help you succeed—anytime, anywhere. Start learning today and take control of your future.</p>
 
-        <p className='md: hidden text-gray-500 max-w-sm mx-auto'> Unlock Your Potential with Expert-Led Courses. Welcome to <b>SkillSync,</b> your all-in-one learning platform where knowledge meets opportunity.</p>
+        <p className='md:hidden text-gray-500 max-w-sm mx-auto'> Unlock Your Potential with Expert-Led Courses. Welcome to <b>SkillSync,</b> your all-in-one learning platform where knowledge meets opportunity.</p>
 
 
         <SearchBar/>
